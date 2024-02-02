9b3a556f048eaefee2e32090765383479536dd32
VC <vivian820225@gmail.com>
2024-02-02 18:10:18 +0800

    Feat: sidebar and modal
    use global context api

---
+++ 12-sidebar-modal/README.md
@@ -0,0 +1,13 @@
+![Sidebar](./demo.png)
+![Modal](./demo-modal.png)
+
+## Figma URL
+
+[Sidebar and Modal](https://www.figma.com/file/cFyEiRb6jQdVIVK9M5eoe6/Sidebar-and-modal?node-id=0%3A1&t=sg6VSjSNK3T1Uy8P-1)
+
+## Usage
+
+```sh
+npm install
+npm run dev
+```
---
+++ 12-sidebar-modal/src/Home.jsx
@@ -0,0 +1,16 @@
+import { useGlobalContext } from './context'
+import { FaBars } from 'react-icons/fa'
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext()
+  return (
+    <main>
+      <button type="button" className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button type="button" className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  )
+}
+export default Home
