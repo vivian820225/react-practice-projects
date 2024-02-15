5931f657ee76980c57f38406ab6895373ad5a29e
VC <vivian820225@gmail.com>
2024-02-15 12:09:04 +0800

    Feat: submenus
    use global context api

---
+++ 13-submenus/README.md
@@ -0,0 +1,12 @@
+![Demo](./demo.png)
+
+## Figma URL
+
+[Strapi Submenus](https://www.figma.com/file/rXeU2gYTyKL2FrYmUNgv4r/Strapi-Submenus?node-id=0%3A1&t=Nx5H36ryj9ArZCI6-1)
+
+## Usage
+
+```sh
+npm install
+npm run dev
+```
---
+++ 13-submenus/src/Submenu.jsx
@@ -0,0 +1,47 @@
+import { useRef } from 'react'
+import { useGlobalContext } from './context'
+import sublinks from './data'
+
+const Submenu = () => {
+  const { pageId, setPageId } = useGlobalContext()
+  const currentPage = sublinks.find((item) => item.pageId === pageId)
+
+  const submenuContainer = useRef(null)
+
+  const handleMouseLeave = (e) => {
+    const submenu = submenuContainer.current
+    const { left, right, bottom } = submenu.getBoundingClientRect()
+    const { clientX, clientY } = e
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null)
+    }
+  }
+
+  return (
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      ref={submenuContainer}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+export default Submenu
