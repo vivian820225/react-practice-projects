44f296f57294919abc97260b0336b0c56a9a9a3f
VC <vivian820225@gmail.com>
2024-03-01 00:04:43 +0800

    Feat: drag and drop - complete the function

--- 21-drag-and-drop/src/components/Column.jsx
+++ 21-drag-and-drop/src/components/Column.jsx
@@ -6,21 +6,93 @@ import AddCard from './AddCard'
 const Column = ({ title, headingColor, cards, column, setCards }) => {
   const [active, setActive] = useState(false)
 
+  const clearHighlights = (els) => {
+    const indicators = els || getIndicators()
+
+    indicators.forEach((indicator) => {
+      indicator.style.opacity = '0'
+    })
+  }
+
+  const highlightIndicator = (e) => {
+    const indicators = getIndicators()
+    clearHighlights(indicators)
+    const nearestEl = getNearestIndicator(e, indicators)
+    nearestEl.element.style.opacity = '1'
+  }
+
+  const getNearestIndicator = (e, indicators) => {
+    const DISTANCE_OFFSET = 50 // for the top one
+
+    const target = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET)
+
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child }
+        } else {
+          return closest
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY, // 負無窮大
+        element: indicators[indicators.length - 1],
+      }
+    )
+
+    return target
+  }
+
+  const getIndicators = () => {
+    return Array.from(document.querySelectorAll(`[data-column="${column}"]`))
+  }
+
   const handleDragStart = (e, card) => {
     e.dataTransfer.setData('cardId', card.id)
   }
 
   const handleDragOver = (e) => {
     e.preventDefault()
+    highlightIndicator(e)
     setActive(true)
   }
 
   const handleDragLeave = (e) => {
+    clearHighlights()
     setActive(false)
   }
 
   const handleDragEnd = (e) => {
+    const targetId = e.dataTransfer.getData('cardId')
+
+    clearHighlights()
     setActive(false)
+
+    const indicators = getIndicators()
+    const { element } = getNearestIndicator(e, indicators)
+    const before = element.dataset.before || '-1'
+
+    if (before !== targetId) {
+      let copy = [...cards]
+      let cardToTransfer = copy.find((card) => card.id === targetId)
+      if (!cardToTransfer) return
+      cardToTransfer = { ...cardToTransfer, column }
+
+      copy = copy.filter((card) => card.id !== targetId)
+
+      const moveToBack = before === '-1' // 是否在最尾端
+
+      if (moveToBack) {
+        copy.push(cardToTransfer)
+      } else {
+        const insertAtIndex = copy.findIndex((card) => card.id === before)
+        if (insertAtIndex === undefined) return
+        copy.splice(insertAtIndex, 0, cardToTransfer)
+      }
+
+      setCards(copy)
+    }
   }
 
   const filteredCards = cards.filter((card) => card.column === column)
